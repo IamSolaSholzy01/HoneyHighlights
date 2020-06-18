44eae96623054087c6dd56be6a38e86a69288e5e
IamSolaSholzy01 <ogolagunju@gmail.com>
2020-06-18 21:26:30 +0100

    Some progress with the validation

--- assets/js/login-register.js
+++ assets/js/login-register.js
@@ -6,10 +6,16 @@
  * Web script: http://creative-tim.com
  * 
  */
-
+ let countErrors = false;
  let clickedStatus = false;
+ let confirmStatus = false;
  let clickNumber = 0;
+ let confirmNumber = 0;
  function aliababwa(){
+    let passError = document.getElementById('passError');
+    if (!countErrors) {
+        passError.style.display = "none";
+    }
     let regpassword = document.getElementById('regpassword');
     let clickShow = document.getElementById('passwordshow');
     if (clickNumber > 0){
@@ -41,26 +47,77 @@
     }
  }
 
+ function alibaba(){
+     let confirmError = document.getElementById('confirmError');
+    if (!countErrors) {
+        confirmError.style.display = "none";
+    }
+    let confirmpassword = document.getElementById('passwordconfirmation');
+    let confirmShow = document.getElementById('confirmpasswordshow');
+    if (confirmNumber > 0){
+        if (!confirmStatus){
+            confirmShow.setAttribute('type', 'text');
+            confirmShow.textContent = "Hide";
+            confirmStatus = true;
+        } 
+    } else if (confirmNumber < 1) {
+        confirmNumber++;
+    }
+
+    confirmShow.style.display = "block";
+    confirmShow.onclick = function (event){
+        event.preventDefault();
+        switch (confirmStatus){
+            case true: 
+                confirmpassword.setAttribute('type', 'password');
+                confirmShow.textContent = "Show";
+                confirmStatus = false;
+                break;
+        
+            default:
+                confirmpassword.setAttribute('type', 'text');
+                confirmShow.textContent = "Hide";
+                confirmStatus = true;
+                break;
+        }
+    }
+ }
+
  window.onclick = function(event){
-    let regpassword = document.getElementById('regpassword');
+    let confirmpassword = document.getElementById('passwordconfirmation');
+    let confirmShow = document.getElementById('confirmpasswordshow');
     let clickShow = document.getElementById('passwordshow');
     let create = document.getElementById('createacc');
     let googleIcon = document.getElementById('google_login');
     let facebookIcon = document.getElementById('facebook_login');
     let logginuser = document.getElementById('logginuser');
-    if (event.target != regpassword && event.target != clickShow && event.target != googleIcon && event.target != facebookIcon && event.target != logginuser && event.target != create  && clickedStatus) {
+    let registerButton = document.getElementById('registerbtn');
+    if (event.target != regpassword && event.target != clickShow && event.target != googleIcon && event.target != facebookIcon && event.target != logginuser && event.target != create && event.target != registerButton && clickedStatus ) {
         event.preventDefault();
         regpassword.setAttribute('type', 'password');
         clickShow.textContent = "Show";
-        clickedStatus = false;
+        clickStatus = false;
         clickShow.style.display = "none";
-    } else if (event.target != regpassword && event.target != clickShow && event.target != googleIcon && event.target != facebookIcon && event.target != logginuser && event.target != create && !clickedStatus) {
+    } else if (event.target != regpassword && event.target != clickShow && event.target != googleIcon && event.target != facebookIcon && event.target != logginuser && event.target != create && event.target != registerButton && !clickedStatus) {
         event.preventDefault();
         regpassword.setAttribute('type', 'password');
         clickShow.textContent = "Show";
-        clickedStatus = true;
+        clickStatus = true;
         clickShow.style.display = "none";
+    } else if (event.target != confirmpassword && event.target != confirmShow && event.target != googleIcon && event.target != facebookIcon && event.target != logginuser && event.target != create  && event.target != registerButton && confirmStatus) {
+        event.preventDefault();
+        confirmpassword.setAttribute('type', 'password');
+        confirmShow.textContent = "Show";
+        confirmStatus = false;
+        confirmShow.style.display = "none";
+    } else if (event.target != confirmpassword && event.target != confirmShow && event.target != googleIcon && event.target != facebookIcon && event.target != logginuser && event.target != create && event.target != registerButton && !confirmStatus) {
+        event.preventDefault();
+        confirmpassword.setAttribute('type', 'password');
+        confirmShow.textContent = "Show";
+        confirmStatus = true;
+        confirmShow.style.display = "none";
     }
+
  }
 
 function showRegisterForm(){
@@ -132,24 +189,45 @@ function validateForm(){
     let confirmPasswordField = document.forms['login']['password_confirmation'].value;
     let passError = document.getElementById('passError');
     let confirmError = document.getElementById('confirmError');
-    var regularExpression = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    var regularExpression = /^(?=.*[0-9])[a-zA-Z0-9]{6,16}$/;
 
     if (passwordField === firstName) {
         passError.textContent = "Do not use your first name as password";
+        passError.style.display = "block";
+        countErrors = true;
         return false;
     } else if (passwordField === "honeyhighlights" || passwordField === "HoneyHighlights" || passwordField === "Honeyhighlights") {
         passError.textContent = "Do not use Honeyhighlights as password";
+        passError.style.display = "block";
+        countErrors = true;
         return false;    
     } else if(passwordField.length < 6){
         passError.textContent = "Password too short. At least 6 characters."
+        passError.style.display = "block";
+        countErrors = true;
+        return false;
     } else if(!regularExpression.test(passwordField)){
-        passError.textContent = "Password must have at least one number and at least one special character.";
+        passError.textContent = "Password must have at least one number.";
+        passError.style.display = "block";
+        countErrors = true;
+        return false;
     } else if(passwordField === surname) {
         passError.textContent = "Do not use yur surname as password";
-    } else if (confirmPasswordField !== passwordField) {
-        confirmError.textContent = "Both passwords must match"        
+        passError.style.display = "block";
+        countErrors = true;
+        return false;
+    } if (confirmPasswordField != passwordField) {
+        confirmError.textContent = "Both passwords must match";
+        confirmError.style.display = "block";
+        countErrors = true;
+        return false;
     }
     else {
+        countErrors = false;
         passError.textContent = "";
+        passError.style.display = "none";
+        confirmError.textContent = "";
+        confirmError.style.display = "none";
+        return true;
     }
 }
---
+++ server.php
@@ -0,0 +1,77 @@
+<?php
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbasename = "honeyhighlights";
+$errors = array();
+
+if($_POST && isset($_POST['firstname'], $_POST['surname'], $_POST['email'], $_POST['password'], $_POST['username'])){
+
+    $firstname = $_POST['firstname'];
+    $surname = $_POST['surname'];
+    $email = $_POST['email'];
+    $usename = $_POST['username'];
+    $passkey = $_POST['password'];
+    
+
+    echo $usename;
+    echo "received input";
+     // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbasename);
+    
+    echo "connection successful";
+     
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+    
+
+    //Check to make sure the username and email are not already existing.
+    $sqls = "SELECT * FROM `user_table` WHERE username = '$usename' OR email = '$email'";
+    $result = mysqli_query($conn, $sqls);
+    $user = mysqli_fetch_assoc($result);
+    if ($user) {
+        echo "<br />name already exists";
+        if ($user['username'] === $usename) {
+            array_push($errors, "Username has been used before.");
+            echo("<br /><h1 style='color:red;'>HIIIII</h1><br/>");
+        }
+        if ($user['email'] === $email) {
+            array_push($errors, "The email already exists. Try logging in?");
+            echo("<br /><h1 style='color:red;'>HIIIII</h1><br/>");
+        }
+    }
+     
+     if (count($errors)==0) {
+        //Hashing function
+        $hashed_password = password_hash($passkey, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO user_table (firstname, surname, email, username, passkey) VALUE ('$firstname','$surname','$email','$usename','$hashed_password')";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "<h1>New record created successfully</h1>";
+            header("Location: blog.html");
+        }
+     }  
+     else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        header("Location: login_register_modal.html");
+    }
+     
+
+    /*
+    if(password_verify($password, $hashed_password)) {
+    
+    } 
+    */
+
+}
+
+if ($_POST && isset($_POST['loginbtn'], $_POST['email'], $_POST['password'])) {
+    $email = $_POST['email'];
+    $passkey = $_POST['email'];
+}
+
+?>
