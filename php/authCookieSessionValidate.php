bd089b5d46527be5c53872ee14dfc18c78b1f494
IamSolaSholzy01 <ogolagunju@gmail.com>
2020-06-22 19:49:38 +0100

    Now, how to put the cookie on the field

--- php/authCookieSessionValidate.php
+++ php/authCookieSessionValidate.php
@@ -27,7 +27,7 @@ else if (! empty($_COOKIE["member_login"]) && ! empty($_COOKIE["random_password"
     $isExpiryDateVerified = false;
     
     // Get token for username
-    $userToken = $auth->getTokenByUsername($_COOKIE["member_login"],0);
+    $userToken = $auth->getTokenByEmail($_COOKIE["member_login"],0);
     
     // Validate random password cookie with database
     if (password_verify($_COOKIE["random_password"], $userToken[0]["password_hash"])) {
