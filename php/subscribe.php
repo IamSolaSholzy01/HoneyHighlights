f65f3f7e160edb2c4e55a837cf5c1ff7d855925d
MISHI RAY <42464035+mishiray@users.noreply.github.com>
2020-07-03 13:54:51 +0100

    php updates for mailer

--- php/subscribe.php
+++ php/subscribe.php
@@ -26,11 +26,48 @@ if($_POST && isset($_POST['email'])) {
             "content" => "Thank you for subscribing.",
             "email" => $email
         );
+
+
+        $to = $email;
+        $from = 'example@honeyhighlights.com'; 
+        $fromName = 'HoneyHighlights'; 
+        $subject = "Subscription";
+        $htmlContent = ' 
+    <html> 
+    <head> 
+        <title>Welcome to Honeyhighlights</title> 
+    </head> 
+    <body> 
+        <h1>Thank you for subscribing to our blog</h1> 
+        <p>You will be notified first of our updates</p>
+        <table cellspacing="0" style="border: 2px dashed #FB4314; width: 100%;"> 
+            <tr> 
+                <th>:</th><td><a href="http://www.honeyhighlights.com">www.honeyhighlights.com</a></td> 
+            </tr> 
+        </table> 
+    </body> 
+    </html>';
+
+    // Set content-type header for sending HTML email 
+    $headers = "MIME-Version: 1.0" . "\r\n"; 
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
+ 
+    // Additional headers 
+    $headers .= 'From: '.$fromName.'<'.$from.'>' . "\r\n"; 
+    // Send email 
+    //if(mail($to, $subject, $htmlContent, $headers)){ 
+    //    echo 'Email has sent successfully.'; 
+    //}else{ 
+    //    echo 'Email sending failed.'; 
+    //}
+
     } else {
 
     }
 }
 }
 echo json_encode($data);
+
+
 
 ?>
